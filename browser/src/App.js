ce9a454ee0b581610990d73a546cebe6eca1f041
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-07 15:34:29 -0400

    Thought I already committed everything, but apparently not

--- browser/src/App.js
+++ browser/src/App.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 function App() {
+
+    useEffect(() => {
+        fetch("/api/test")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }, []);
+
     return (
         <>
             <h1>MERN App</h1>
--- browser/src/index.js
+++ browser/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+    // Every time you load the page with useEffect, this will make every component run twice. It's how it shows errors.
     <React.StrictMode>
         <App />
     </React.StrictMode>
