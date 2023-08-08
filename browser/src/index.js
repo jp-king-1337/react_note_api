bc87107d10b133967bc891f3bbdbdf637d414ada
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-08 13:23:31 -0400

    No errors now. Had some issues in browser/src/index.js

--- browser/src/index.js
+++ browser/src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
+
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     // Every time you load the page with useEffect, this will make every component run twice. It's how it shows errors.
     <React.StrictMode>
-        <browser>
+        <BrowserRouter>
             <App />
-        </browser>
+        </BrowserRouter>
     </React.StrictMode>
 );
