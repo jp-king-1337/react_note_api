7b4153d07071d4fdc02b15b53bbd9806eb6ed120
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-08 11:17:51 -0400

    Diverged from class a bit by using index.js in models folder to export models

---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+import { Note } from "./model/Note";
+import { User } from "./model/User";
+
+export { Note, User };
--- server/routes/api_routes.js
+++ server/routes/api_routes.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
-const { createToken, validateToken } = require("../auth")
+const { createToken, validateToken } = require("../auth");
+
+const { Note, User } = require("../models/index");
 
 // User routes
+router.post("/register", async (req, res) => {
+    try {
+        const user = await User.create(req.body);
 
+        const token = await createToken(user_id);
+
+        res.cookie
+    }
+    catch(err) {
+
+    }
+});
 
 // Note routes
 
