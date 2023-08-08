855badf2a98531349be94a7f941e76ddcabec499
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-08 10:51:53 -0400

    Added User and Note models

---
+++ server/models/Note.js
@@ -0,0 +1,14 @@
+const { Schema, model, Types } = require("mongoose");
+
+const noteSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Types.ObjectId,
+        ref: "User"
+    }
+}, {
+    timestamps: true
+});
