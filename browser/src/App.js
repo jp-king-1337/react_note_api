c556ebd5c8ef37ff0d27846ad6c5178e8f5bdc2e
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-08 13:19:29 -0400

    Going to add sass

--- browser/src/App.js
+++ browser/src/App.js
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 
+// Components
+import Loading from "./components/Loading";
+
+// Pages
 import AuthForm from "./pages/AuthForm";
 
 function App() {
     const [appData, setAppData] = useState({
-
-    })
+        user: null,
+        notes: [],
+        loading: true
+    });
     // useEffect(() => {
     //     fetch("/api/test")
     //         .then(res => res.json())
@@ -18,7 +24,7 @@ function App() {
         <>
             <h1>Note App</h1>
 
-            <AuthForm />
+            {appData.loading && <Loading />}
         </>
     );
 }
