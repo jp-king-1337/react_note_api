369fdfc23a9829a633420c674dd55e68eb0ed09d
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-08 14:20:03 -0400

    Colors and other styling

--- browser/src/pages/AuthForm.js
+++ browser/src/pages/AuthForm.js
@@ -14,11 +14,11 @@ export default function AuthForm(props) {
     return (
         <>
             <h1 className="text-center">Log In</h1>
-            <form>
+            <form className="column">
                 <input type="email" placeholder="Enter your email" />
                 <input type="password" placeholder="Enter your password" />
                 <button>Submit</button>
-                <div className="toggle-wrap" style={styles.toggleWrap}>
+                <div className="toggle-wrap row justify-center" style={styles.toggleWrap}>
                     <label style={styles.label} htmlFor="login">Login</label>
                     <input style={styles.input} type="radio" id="login" value="login" checked={true} />
                     <label style={styles.label} htmlFor="register">Register</label>
